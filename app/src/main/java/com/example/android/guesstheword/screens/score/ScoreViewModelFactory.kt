715ac60d388458261bdad935cdb6dc09bec6e5f6
Stella Nguyen <stellakimnguyen@gmail.com>
2021-05-21 11:46:24 -0400

    Refactor starter app to implement MVVM

---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModelFactory.kt
@@ -0,0 +1,10 @@
+package com.example.android.guesstheword.screens.score
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class ScoreViewModelFactory(private val finalScore : Int) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return ScoreViewModel(finalScore) as T
+    }
+}
